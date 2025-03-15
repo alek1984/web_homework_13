2c4db78f8ff0053807378f2ba329f000c538f8b2
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-15 16:14:52 +0200

    Update cloudinary.py

--- cloudinary.py
+++ cloudinary.py
@@ -1,5 +1,10 @@
 import cloudinary.uploader
 
-def upload_avatar(file):
-    result = cloudinary.uploader.upload(file.file)
-    return result["url"]
+@router.post("/upload-avatar")
+async def upload_avatar(user_id: int, file: UploadFile = File(...), db=Depends(get_db)):
+    upload_result = cloudinary.uploader.upload(file.file)
+    user = db.query(User).filter(User.id == user_id).first()
+    user.avatar_url = upload_result["url"]
+    db.commit()
+    return {"avatar_url": upload_result["url"]}
+
