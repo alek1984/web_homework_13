577a7918990a6bda2cd9ddbda2e4242f2e6bd3fb
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-15 16:11:30 +0200

    Update cors.py

--- cors.py
+++ cors.py
@@ -1,9 +1,15 @@
 from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "https://mytrusteddomain.com",
+    "https://another-trusted.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
