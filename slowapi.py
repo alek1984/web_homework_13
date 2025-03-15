6d57af4d6ad07ad445c5f58a50b0de58e352e01b
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-15 16:08:50 +0200

    Update slowapi.py

--- slowapi.py
+++ slowapi.py
@@ -1,6 +1,15 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from fastapi import FastAPI, Depends
+from fastapi.routing import APIRouter
 
+app = FastAPI()
 limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
 
-@app.post("/contacts", dependencies=[Depends(limiter.limit("5/minute"))])
+router = APIRouter()
+
+@router.post("/contacts", dependencies=[Depends(limiter.limit("5/minute"))])
+async def create_contact():
+    return {"message": "Контакт створено"}
+
