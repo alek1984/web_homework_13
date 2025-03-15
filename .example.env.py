e3fcd2b2b1bc053f9aaded7cce8a6fcf876132f9
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-15 16:52:16 +0200

    Update and rename dotenv.py to .example.env.py

---
+++ .example.env.py
@@ -0,0 +1,13 @@
+MAIL_USERNAME=your_email@gmail.com
+MAIL_PASSWORD=your_password
+MAIL_FROM=your_email@gmail.com
+MAIL_PORT=587
+MAIL_SERVER=smtp.gmail.com
+MAIL_TLS=True
+MAIL_SSL=False
+REDIS_HOST=localhost
+REDIS_PORT=6379
+CLOUDINARY_CLOUD_NAME=your_cloud_name
+CLOUDINARY_API_KEY=your_api_key
+CLOUDINARY_API_SECRET=your_api_secret
+
